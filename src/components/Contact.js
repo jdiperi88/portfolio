59e1174ef561cc9c8e5bcf5b03291431d114c04e
jdiperi88 <jdiperi88@yahoo.com>
2018-07-21 18:48:30 -0400

    added netlify form fields

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom'
 class Contact extends Component {
+    state ={
 
-    handleFormSubmit= (e)=>{
-        e.preventDefault()
+    }
+    handleInputChange=()=>{
+
     }
     render(){
         let {
@@ -14,20 +16,21 @@ class Contact extends Component {
                     <div className='contact-photo'>
                         <img src={require('../images/keyboard.jpg')}/>
                     </div>
-                    <form>
+                    <form name="contact" method="POST" netlify>
                         <h1>CONTACT</h1>
                         <div className='field-container'>
                             <div className='row'>
-                                <input type='text' name='name' placeholder='Name' />
+                                <input type='text' name='name' placeholder='Name' required />
                                 <input type='text' name='company' placeholder='Company' />
                             </div>
                             <div className='row'>
-                                <input type='text' name='email' placeholder='Email' />
-                                <input type='text' name='subject' placeholder='Subject' />
+                                <input type='email' name='email' placeholder='Email' required />
+                                <input type='text' name='subject' placeholder='Subject' required />
                             </div>
-                            <textarea placeholder='Message' rows="10" cols="50" />
+                            <textarea placeholder='Message' rows="10" cols="50" required />
                         </div>
-                        <button onClick={(e)=>this.handleFormSubmit(e)} className='btn' type='submit'>Submit</button>
+                        <div data-netlify-recaptcha></div>
+                        <button className='btn' type='submit'>Submit</button>
                     </form>
                 </section>
                 
