f38e71741813192f34d46306fc2ad2d8b5ad2ba0
jdiperi88 <jdiperi88@yahoo.com>
2018-11-23 13:03:10 -0500

    modified app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,52 @@
-import React, { Component } from 'react';
-import '../styles/main.css';
-import Header from './Header'
-import Footer from './Footer'
-import Contact from './Contact'
-import Home from './Home'
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import ProjectContainer from './ProjectContainer';
-import Carousel from './Carousel';
+import React, { Component } from "react";
+import "../styles/main.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Contact from "./Contact";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ProjectContainer from "./ProjectContainer";
+import Carousel from "./Carousel";
 class App extends Component {
-  state ={
-    navIsOpen:'',
-    active:'',
-  }
-  openNav=()=>{
-    this.setState({
-      navIsOpen: this.state.navIsOpen == ''?'open':'',
-      active: this.state.active == ''?'active':''
+	state = {
+		navIsOpen: "",
+		active: ""
+	};
+	openNav = () => {
+		this.setState({
+			navIsOpen: this.state.navIsOpen == "" ? "open" : "",
+			active: this.state.active == "" ? "active" : ""
+		});
+	};
 
-  })
-  }
+	closeNav = () => {
+		this.setState({
+			navIsOpen: "",
+			active: ""
+		});
+	};
 
-  closeNav=()=>{
-    this.setState({
-      navIsOpen: '',
-      active:''
-    })
-  }
-
-  
-  render() {
-    const { navIsOpen, active} = this.state
-    return (
-      <Router>
-      <div className="App">
-        <Header
-          navIsOpen={navIsOpen}
-          active={active}
-          openNav={this.openNav}
-          closeNav={this.closeNav}
-        />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/Projects' component={ProjectContainer} />
-        <Route exact path='/Contact' component={Contact} />
-      </Switch>
-        <Footer />
-      </div>
-      </Router>
-    );
-  }
+	render() {
+		const { navIsOpen, active } = this.state;
+		return (
+			<Router>
+				<div className="App">
+					<Header
+						navIsOpen={navIsOpen}
+						active={active}
+						openNav={this.openNav}
+						closeNav={this.closeNav}
+					/>
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/Projects" component={ProjectContainer} />
+						<Route exact path="/Contact" component={Contact} />
+					</Switch>
+					<Footer />
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
