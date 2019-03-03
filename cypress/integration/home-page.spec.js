2c6b1037cb6ac45c8c3e1d85865ba32b5ce7260b
jdiperi88 <jdiperi88@yahoo.com>
2019-03-02 20:37:04 -0500

    added cypress and setup three integration tests for the home page

---
+++ cypress/integration/home-page.spec.js
@@ -0,0 +1,25 @@
+describe("Home Page tests", () => {
+  it("should have linked in button and link to linked in", () => {
+    cy.visit("http://localhost:3001/");
+    cy.get(".home-linked-in-link")
+      .should("have.attr", "href")
+      .and("include", "https://www.linkedin.com/in/jdiperi");
+    cy.get(".home-linked-in-link").click();
+  });
+
+  it("should have github in button and link to github in", () => {
+    cy.visit("http://localhost:3001/");
+    cy.get(".home-github-link")
+      .should("have.attr", "href")
+      .and("include", "https://github.com/jdiperi88");
+    cy.get(".home-github-link").click();
+  });
+
+  it("should have resume in button and link to resume in", () => {
+    cy.visit("http://localhost:3001/");
+    cy.get(".home-resume-link")
+      .should("have.attr", "href")
+      .and("include", "jdiperi_resume.pdf");
+    cy.get(".home-resume-link").click();
+  });
+});
